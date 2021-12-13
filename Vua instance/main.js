e60e0cd1b2e1b0b24a51007f04a4557354cd1bab
Trung Can <cannguyen2812@gmail.com>
2021-12-13 10:33:32 +0700

    commit Thư mục mới

---
+++ Vua instance/main.js
@@ -0,0 +1,24 @@
+// var vueInstance = new Vue({
+//     el: "#app",
+//     data: {
+//         tittle: "Điện Thoại Samsung"
+//     },
+//     methods: {
+//         say: function(text){
+//             return 'Hello' + text;
+//         }
+//     }
+// });
+// console.log(vueInstance);
+
+
+var obj = {
+    foo: 'bar'
+  }
+  
+//   Object.freeze(obj)
+  
+  new Vue({
+    el: '#vd2',
+    data: obj
+  })
