338868990f798d778513d126c18b459bf3f961a2
Trung Can <cannguyen2812@gmail.com>
2021-12-14 10:55:48 +0700

    Commit 2

--- Vua instance/main.js
+++ Vua instance/main.js
@@ -12,13 +12,34 @@
 // console.log(vueInstance);
 
 
-var obj = {
-    foo: 'bar'
+// var obj = {
+//     foo: 'bar'
+//   }
+  
+// //   Object.freeze(obj)
+  
+//   new Vue({
+//     el: '#vd2',
+//     data: obj
+//   })
+
+// Ví dụ 3: 
+var app = new Vue({
+  el: "#app",
+  data: {
+    counter: 0,
+    clientX: 0,
+    clientY: 0,
+  },
+  methods:{
+    bamnut(e,number){
+      console.log("đã click",e);         //e = event
+      this.counter +=number;
+    },
+    handleMouseMove(e) {
+      console.log("đã di chuột",e);
+      this.clientX = e.clientX;
+      this.clientY = e.clientY;
+    },
   }
-  
-//   Object.freeze(obj)
-  
-  new Vue({
-    el: '#vd2',
-    data: obj
-  })
+})
